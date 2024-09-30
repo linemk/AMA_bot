1a1466746113c1b1e92731827cb963195467f4bf
Максим Тверитинов <makstveritinow0809@gmail.com>
2024-09-30 20:35:21 +0300

    struct fixes

--- pkg/telegramClient/answer.go
+++ pkg/telegramClient/answer.go
@@ -1,4 +1,4 @@
-package main
+package telegramClient
 
 import (
 	weather "AMA_bot/pkg/weatherAPI" // Импортируйте пакет с погодой
--- pkg/telegramClient/getUpdates_test.go
+++ pkg/telegramClient/getUpdates_test.go
@@ -1,4 +1,4 @@
-package main
+package telegramClient
 
 import (
 	"flag"
