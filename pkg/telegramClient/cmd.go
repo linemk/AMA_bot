c61e6c2aabc76800063fe3ca34c4d3f84852d19b
Максим Тверитинов <makstveritinow0809@gmail.com>
2024-10-01 07:44:56 +0300

    small bug fixes/ru lang

--- pkg/telegramClient/cmd.go
+++ pkg/telegramClient/cmd.go
@@ -4,7 +4,7 @@ import "fmt"
 
 // команда для /start
 func StartParser(client *Client, Id int, name string) {
-	var text string = fmt.Sprintf("Привет, %s!\nЧтобы узнать погоду в городе - введи название города на английском\n\nПример: Москва -> Moscow", name)
+	var text string = fmt.Sprintf("Привет, %s!\nЧтобы узнать погоду в городе -> введи название города\n\nПример: Москва", name)
 	err := client.SendMessage(Id, text)
 	if err != nil {
 		_ = fmt.Errorf("can't send message")
